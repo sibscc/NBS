c622c8a699d0de34da37540c3b49535b64451c55
MaChengcheng <ccma@smartquerier.com>
2017-03-03 20:08:59 +0800

    Vcf2Matrix.R can deal with gVCF ignoring the homo or heter mutations

--- Vcf2Matrix.R
+++ Vcf2Matrix.R
@@ -43,13 +43,6 @@ if(!file.exists(input)){
   q(status=1)
 }
 
-
-files = dir(input, pattern="*.vcf")
-IDs <- fread(network, select=1)[[1]]
-out <- matrix(0, length(IDs), length(files))
-colnames(out) <- files
-rownames(out) <- IDs
-
 #### Load EntrezID to gene
 x <- org.Hs.egSYMBOL2EG
 xx <- mappedkeys(x)
@@ -57,47 +50,111 @@ mapped_genes <- mappedkeys(x)
 xx <- as.list(x[mapped_genes])
 ####### get the nonsynonymous mutated genes in a vcf file
 cat("Data loaded. Start ...!!!\n")
-if(germline == F){
-    for(i in 1:length(files)){
-	  f = read.table(file.path(input, files[i]))
-	  non <- grep("nonsynonymous",f[,8],value=T)
-	  nn <- unlist(strsplit(non, ";"))
-	  refgene <- grep("Gene.refGene=", nn, value=T)
-	  refgene <- unique(refgene)
-	  gene <- unlist(strsplit(refgene, "="))[seq(2, 2*length(refgene), 2)]
-	  ############## Search in org.Hs.eg.fb
-	  #gene <- unlist(strsplit(gene, ","))
-	  #gene <- unlist(strsplit(gene, "\\\\x3b"))
-	  gene <- unique(gene)
-	  yy <- unlist(xx[gene])
-	  zz <- intersect(rownames(out), yy)
-	  out[zz, files[i]] <- 1
-	}
-}else{
-    for(i in 1:length(files)){
-          f = read.table(file.path(input, files[i]))
-          non <- grep("nonsynonymous",f[,8],value=T)
-	  genelist <- numeric()
-          for(j in 1:length(non)){
-		item <- strsplit(non[j], ";")[[1]]
-		AC <- strsplit(grep("^AC=",item, value=T), "=")[[1]][2]
-		gene <-  strsplit(grep("^Gene.refGene=",item, value=T), "=")[[1]][2]
-		if(AC ==1){
-			if(!(gene %in% names(genelist))){
-				genelist[gene] <- 1
-			}
-		}else if(AC == 2){
-			genelist[gene] <- 2
+if(gvcf == F){
+	samples <- dir(input, pattern="*.vcf")
+	IDs <- fread(network, select=1)[[1]]    ## read the network matrix to get the rownames of VCF matrix
+	out <- matrix(0, length(IDs), length(files))
+	colnames(out) <- files
+	rownames(out) <- IDs
+	if(germline == F){
+	    for(i in 1:length(samples)){
+		  f = read.table(file.path(input, files[i]))
+		  non <- grep("nonsynonymous",f[,8],value=T)
+		  nn <- unlist(strsplit(non, ";"))
+		  refgene <- grep("Gene.refGene=", nn, value=T)
+		  refgene <- unique(refgene)
+		  gene <- unlist(strsplit(refgene, "="))[seq(2, 2*length(refgene), 2)]
+		  ############## Search in org.Hs.eg.fb
+		  #gene <- unlist(strsplit(gene, ","))
+		  #gene <- unlist(strsplit(gene, "\\\\x3b"))
+		  gene <- unique(gene)
+		  yy <- unlist(xx[gene])
+		  zz <- intersect(rownames(out), yy)
+		  out[zz, files[i]] <- 1
 		}
-	  }
-	  Symbo2Id <- xx[names(genelist)]
-	  Symbo2Id[sapply(Symbo2Id, is.null)] <- NA
-	  EZs <- unlist(Symbo2Id)
-	  names(genelist) <- EZs
-          ############## Search in org.Hs.eg.fb
-          zz <- intersect(rownames(out), EZs)
-          out[zz, files[i]] <- genelist[zz]
+	}else{
+	    for(i in 1:length(samples)){
+	          f = read.table(file.path(input, files[i]))
+	          non <- grep("nonsynonymous",f[,8],value=T)
+		  genelist <- numeric()
+	          for(j in 1:length(non)){
+			item <- strsplit(non[j], ";")[[1]]
+			AC <- strsplit(grep("^AC=",item, value=T), "=")[[1]][2]
+			gene <-  strsplit(grep("^Gene.refGene=",item, value=T), "=")[[1]][2]
+			if(AC ==1){
+				if(!(gene %in% names(genelist))){
+					genelist[gene] <- 1
+				}
+			}else if(AC == 2){
+				genelist[gene] <- 2
+			}
+		  }
+		  Symbo2Id <- xx[names(genelist)]
+		  Symbo2Id[sapply(Symbo2Id, is.null)] <- NA
+		  EZs <- unlist(Symbo2Id)
+		  names(genelist) <- EZs
+	          ############## Search in org.Hs.eg.fb
+	          zz <- intersect(rownames(out), EZs)
+	          out[zz, files[i]] <- genelist[zz]
+	        }
+	}
+	write.table(out, file=output,sep="\t", col.names=NA)
+}else{
+    files <- read.table(input, header=T,fill=T,sep="\t")
+    start = which(colnames(files) == "FORMAT")
+    samples <- colnames(files)[(start + 1) : ncol(files)]
+    IDs <- fread(network, select=1)[[1]]    ## read the network matrix to get the rownames of VCF matrix
+    out <- matrix(0, length(IDs), length(samples))
+    colnames(out) <- samples
+    rownames(out) <- IDs
+    splicing_i <-grep("splicing", files[,"Func.refGene"])     # use genes with splicing change or nonsynonymous change
+        nonsy_i <- grep("nonsyn", files[,"ExonicFunc.refGene"])
+        stop_i <- grep("stopgain", files[,"ExonicFunc.refGene"])
+        frame_i <- grep("frameshift", files[,"ExonicFunc.refGene"])
+        change_i <- unique(c(splicing_i, nonsy_i, stop_i, frame_i))
+        change_m <- files[change_i, ]
+        genes <- unique(change_m[,"Gene.refGene"])
+        geneIndex <- list()
+	for(i in as.character(genes)){
+                geneIndex[[i]] <- which(i == change_m[,"Gene.refGene"])
         }
+    if(germline == F){						  ## gVCF somatic mutation or not consider homo or heter mutation
+	for(i in samples){
+		gene <- character()
+		for(j in names(geneIndex)){
+			gt <- change_m[geneIndex[[j]], i]
+			homo <- grep("0/0", gt)
+			unk <- grep("\\./\\.", gt)
+			if(length(homo) + length(unk) < length(gt)){
+				gene <- c(gene, j)
+			}
+		}
+		yy <- unlist(xx[gene])
+                zz <- intersect(rownames(out), yy)
+                out[zz, i] <- 1
+	}
+    }else{
+	genelist <- numeric()
+		
+
+    }    
 }
-write.table(out, file=output,sep="\t", col.names=NA)
+
+
 cat("VCF to Matrix Done!!!\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
